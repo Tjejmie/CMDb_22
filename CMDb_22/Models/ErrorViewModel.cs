c6569b3cebe3b777c4358e6bfab46b84e8a253c4
jamie <jamie_svanberg@hotmail.com>
2021-10-20 09:45:57 +0200

    Add project files.

---
+++ CMDb_22/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CMDb_22.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
